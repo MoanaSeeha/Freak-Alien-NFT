d9700d655e681c92f74454863f7361926d305ac0
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-04-27 00:36:55 +0800

    ff

--- frontend/src/pages/main/Container.tsx
+++ frontend/src/pages/main/Container.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Container = () => {
   return (
-    <div className="containerr mx-auto hero pb-10 px-4">
-      <img src={`${process.env.PUBLIC_URL}/image/fac-background.svg`} className=" w-full"/>
-     
+    <div className="mx-auto hero pb-10 flex flex-col justify-center">
+      <img src={`${process.env.PUBLIC_URL}/image/fac-background.svg`} className=" w-full absolute"/>
+      <div className=" flex flex-col px-24 z-10 text-6xl font-extrabold">
+        <div className=" p-4 bg-blue-700 text-2xl font-bold inline-block w-fit my-8">
+          WELCOME TO
+        </div>
+        Freak Aliens Club
+      </div>
     </div>
   )
 }
